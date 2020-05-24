7febf82495902601b78a5db35233c10a56659344
Dombi Tibor <dombidav@gmail.com>
2020-05-24 07:19:25 +0200

    task view

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\LINQ;
+use App\Project;
 use App\Task;
+use App\User;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
 
 class TaskController extends Controller
 {
@@ -19,25 +27,87 @@ class TaskController extends Controller
     }
 
     /**
+     * @param $target
+     * @param Collection $tasks
+     * @return \Illuminate\Support\Collection
+     */
+    private static function target_filter($target, Collection $tasks)
+    {
+        if ($target == '$')
+            $target = Auth::user()->username;
+        $temp = [];
+        /** @var Task $task */
+        foreach ($tasks as $task) {
+            if (Task::owner_of($task) != null && Task::owner_of($task)->username == $target) {
+                array_push($temp, $task);
+            }
+        }
+        return collect($temp);
+    }
+
+    /**
+     * @param $filter
+     * @param Collection $tasks
+     * @return Collection
+     */
+    private static function status_filter($filter, $tasks)
+    {
+        switch ($filter) {
+            case 'active':
+                $tasks = $tasks->where('status', '<>', '0')->where('completed', '==', null);
+                break;
+            case 'done':
+                $tasks = $tasks->where('completed', '<>', null);
+                break;
+            case 'backlog':
+                $tasks = $tasks->where('status', '==', '0')->where('completed', '==', null);
+                break;
+        }
+        return $tasks;
+    }
+
+    /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     * @return Factory|Response|View
      */
     public function index()
     {
-        if(\request('p') != null){
-
-        }else if(Auth::user()->auth == 9){
+        /** @var Collection $tasks */
+        if (\request('p') != null) {
+            $tasks = Task::where('project_id', '=', \request('p'))->get();
+            if (\request('filter') != null) {
+                foreach (\request('filter') as $filter) {
+                    switch (explode(' ', $filter)[0]) {
+                        case 'status':
+                            $tasks = self::status_filter(explode(' ', $filter)[1], $tasks);
+                            break;
+                        case 'target':
+                            $tasks = self::target_filter(explode(' ', $filter)[1], $tasks);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            //$tasks = LINQ::paginate($tasks);
+            return view('task.index', ['tasks' => $tasks, 'project' => Project::find(request('p'))]);
+        } else if (Auth::user()->auth == 9) {
             return view('task.index_admin', ['user' => Auth::user(), 'tasks' => Task::query()->paginate(20)]);
-        }else{
+        } else {
             abort(403, 'Task view only available for administrators');
         }
+    }
+
+    public function apiShow($id)
+    {
+        return json_encode(Task::find($id));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -47,8 +117,8 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -58,8 +128,8 @@ class TaskController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param \App\Task $task
+     * @return Response
      */
     public function show(Task $task)
     {
@@ -69,8 +139,8 @@ class TaskController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param \App\Task $task
+     * @return Response
      */
     public function edit(Task $task)
     {
@@ -80,23 +150,52 @@ class TaskController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Task $task
+     * @return \Illuminate\Http\RedirectResponse|Response
      */
-    public function update(Request $request, Task $task)
+    public function update(Request $request, Task $task = null)
     {
-        //
+        if ($task == null)
+            $task = Task::find($request->get('task_id'));
+        if(Auth::user()->auth != 9 && $task->owner()->id != Auth::id())
+            abort(403, 'Only the owner can edit this item');
+        if ($request->get('new_title'))
+            $task->title = $request->get('new_title');
+        if ($request->get('new_content') !== null)
+            $task->content = $request->get('new_content');
+        if ($request->get('new_deadline') !== null)
+            $task->deadline = $request->get('new_deadline');
+        if ($request->get('new_status') !== null)
+            $task->status = $request->get('new_status');
+        $task->save();
+
+        foreach (explode(',', $request->get('new_responsibles')) as $person) {
+            $user = User::name(trim($person, ', '));
+            DB::table('user_task')->where('task_id', '=', $task->id)->delete();
+            DB::table('user_task')->insert([
+                'user_id' => $user->id,
+                'task_id' => $task->id
+            ]);
+        }
+
+        return redirect()->route('tasks.index', ['p' => $task->project->id]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param \App\Task $task
+     * @return \Illuminate\Http\RedirectResponse|Response
+     * @throws \Exception
      */
     public function destroy(Task $task)
     {
-        //
+        if ($task->owner()->id == Auth::id() || Auth::user()->auth == 9) {
+            $temp = $task->project->id;
+            $task->delete();
+            return redirect()->route('tasks.index', $temp);
+        } else
+            abort(403, 'Only the owner can delete this item');
     }
 }
---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,217 @@
+@php
+    use App\Project;use App\Task;use Illuminate\Pagination\LengthAwarePaginator;
+    /** @var Project $project
+    *   @var LengthAwarePaginator $tasks
+    *   @var Task $task
+    */
+@endphp
+
+
+@extends('layouts.app', ['active_page' => 'project', 'page_title' => 'My Projects'])
+
+@push('style')
+    <link rel="stylesheet" type="text/css"
+          href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.21/af-2.3.5/b-1.6.2/b-html5-1.6.2/b-print-1.6.2/cr-1.5.2/fc-3.3.1/fh-3.1.7/kt-2.5.2/r-2.2.5/rr-1.2.7/sc-2.0.2/sp-1.1.1/sl-1.3.1/datatables.min.css"/>
+    <link rel="stylesheet" href="{{ asset('css/project.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/amsify.suggestags.css') }}"/>
+@endpush
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <table id="task_table" class="table display">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Responsible Persons</th>
+                    <th>Created by</th>
+                    <th>Deadline</th>
+                    <th>Status</th>
+                    <th>Created At</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($tasks as $task)
+                    <tr>
+                        <td>
+                            {{ Task::innerID($task) }}
+                        </td>
+                        <td>
+                            <a href="#" data-toggle="modal" data-target="#task_edit_modal" data-task_id="{{ $task->id }}">{{ $task->title }}</a>
+                        </td>
+                        <td>
+                            @foreach($task->users as $u)
+                                <a href="{{ route('user.show', $u->username) }}">{{ $u->username }}</a>,
+                            @endforeach
+                            <input type="hidden" id="{{ $task->id }}_responsibles" value="{{ \App\Task::userString($task) }}">
+                        </td>
+                        <td>
+                            <a href="{{ route('user.show', $task->owner()->username) }}">{{ $task->owner()->username }}</a>
+                        </td>
+                        <td>
+                            {{ $task->deadline }}
+                        </td>
+                        <td>
+                            @switch($task->status)
+                                @case('0')
+                                    <p>Backlog</p>
+                                    @break
+                                @case('1')
+                                <p>On Hold</p>
+                                @break
+                                @case('2')
+                                <p>Planning</p>
+                                @break
+                                @case('3')
+                                <p>In Progress</p>
+                                @break
+                                @case('4')
+                                <p>Verifying</p>
+                                @break
+                                @case('5')
+                                <p>On Review</p>
+                                @break
+                                @default
+                                <p>Completed at {{ $task->completed }}</p>
+                                @break
+                            @endswitch
+                        </td>
+                        <td>
+                            {{ $task->created_at }}
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="modal fade" id="task_edit_modal" tabindex="-1" role="dialog" aria-labelledby="task_edit_label" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="task_edit_label">Task data</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form name="task_edit_form" id="task_edit_form" action="{{ route('api.task.update') }}" method="post">
+                        @csrf
+                        @method('put')
+                        <input type="hidden" name="task_id" id="task_id" value="">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <label for="new_title" class="col-form-label">Title:</label>
+                                    <input type="text" required="required" class="form-control" name="new_title" id="new_title">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="new_status" class="col-form-label">Status:</label>
+                                    <select class="form-control" name="new_status" id="new_status">
+                                        <option value="0">Backlog</option>
+                                        <option value="1">On Hold</option>
+                                        <option value="2">Planning</option>
+                                        <option value="3">In Progress</option>
+                                        <option value="4">Verifying</option>
+                                        <option value="5">On Review</option>
+                                        <option value="9">Completed</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <label for="new_responsibles" class="col-form-label">Responsible persons:</label>
+                                    <input type="text" class="form-control" name="new_responsibles" id="new_responsibles">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="new_deadline" class="col-form-label">Deadline:</label>
+                                    <input type="datetime-local" class="form-control" name="new_deadline" id="new_deadline">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="new_content" class="col-form-label">Description:</label>
+                                    <textarea class="form-control" name="new_content" id="new_content"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" onclick="deleteClick();">Delete</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" form="task_edit_form" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script src="{{ asset('js/jquery.amsify.suggestags.js') }}"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript"
+            src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.21/af-2.3.5/b-1.6.2/b-html5-1.6.2/b-print-1.6.2/cr-1.5.2/fc-3.3.1/fh-3.1.7/kt-2.5.2/r-2.2.5/rr-1.2.7/sc-2.0.2/sp-1.1.1/sl-1.3.1/datatables.min.js"></script>
+    <script>
+        let active_task = 0;
+        $(document).ready(function() {
+            $('#task_table').DataTable();
+            $('#task_edit_modal').on('show.bs.modal', function (event) {
+                active_task = $(event.relatedTarget).data('task_id');
+                $.ajax({
+                    url: '/api/t/' + active_task,
+                    dataType: 'json',
+                    type: 'get',
+                    contentType: 'application/x-www-form-urlencoded',
+                    data: { },
+                    success: function( data, textStatus, jQxhr ){
+                        $('#new_title').val(data?.title);
+                        $('#new_status').val(data?.status);
+                        $('#new_content').val(data?.content);
+                        $('#new_deadline').val(data?.deadline);
+                        $('#task_id').val(active_task);
+                        //$('#new_responsibles').val($('#' + active_task + '_responsibles').val());
+                        active_task = active_task;
+                    },
+                    error: function( jqXhr, textStatus, errorThrown ){
+                        console.log(jqXhr.message);
+                    }
+                });
+            })
+            $('#new_responsibles').amsifySuggestags({
+                suggestions: [
+                    @foreach(\App\User::all() as $user)
+                        '{{ $user->username }}',
+                    @endforeach
+                ]
+            });
+        } );
+
+        function deleteClick(){
+            $.ajax({
+                url: '/t/' + active_task,
+                dataType: 'json',
+                type: 'delete',
+                contentType: 'application/x-www-form-urlencoded',
+                data: { },
+                success: function( data, textStatus, jQxhr ){
+                    location.refresh();
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log(jqXhr.message);
+                }
+            });
+        }
+    </script>
+@endpush
