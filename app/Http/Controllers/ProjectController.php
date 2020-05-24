c3bae2aced27012efc77f0402ab109c17e7726ab
Dombi Tibor <dombidav@gmail.com>
2020-05-24 23:20:49 +0200

    Project creation

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -53,25 +54,13 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        $project = new Project([
-            'name' => $request->get('new_name'),
-            'deadline' => $request->get('new_deadline'),
-            'user_id' => Auth::id(),
-            'description' => $request->get('new_content')
-        ]);
-        $project->save();
-        $project = Project::last();
-        DB::table('user_project')->insert([
-            'user_id' => Auth::id(),
-            'project_id' => $project->id
-        ]);
-        if(Str::contains($request->get('new_responsibles'), ',')){
-            foreach (explode(',', $request->get('new_responsibles')) as $person){
-                DB::table('user_project')->insert([
-                    'user_id' => $user = User::name($person)->id,
-                    'project_id' => $project->id
-                ]);
-            }
+        switch ($request->get('create_method')) {
+            case 'manual':
+                $this->manual_create($request);
+                break;
+            case 'trello':
+                $this->trello_create($request);
+                break;
         }
         return redirect()->route('projects.index');
     }
@@ -98,38 +87,40 @@ class ProjectController extends Controller
         return view('project.edit', ['project' => $project]);
     }
 
-    public function memberRemove(Request $request){
+    public function memberRemove(Request $request)
+    {
         $project = Project::find($request->get('project'));
         $user = User::find($request->get('user'));
-        if($project->user_id == Auth::id() || Auth::user()->auth == 9){
-            if($user->id == Auth::id()){
+        if ($project->user_id == Auth::id() || Auth::user()->auth == 9) {
+            if ($user->id == Auth::id()) {
                 abort(400, 'You are the owner of this project');
-            }else{
+            } else {
                 DB::table('user_project')->where('user_id', '=', $user->id)->where('project_id', '=', $project->id)->delete();
                 return redirect()->route('projects.show', $project->id);
             }
-        }else{
+        } else {
             abort(403, 'Only the owner can remove this person from the project');
         }
     }
 
-    public function memberAdd(Request $request){
+    public function memberAdd(Request $request)
+    {
         $project = Project::find($request->get('project'));
-        if($project->user_id == Auth::id() || Auth::user()->auth == 9){
+        if ($project->user_id == Auth::id() || Auth::user()->auth == 9) {
             $request->validate(['new_member' => 'required']);
-            if(Str::contains($request->get('new_member'), ',')){
+            if (Str::contains($request->get('new_member'), ',')) {
                 foreach (explode(',', $request->get('new_member')) as $user) {
                     $user = User::name($user);
-                    if(DB::table('user_project')->where('user_id', '=', $user->id)->where('project_id', '=', $project->id)){
+                    if (DB::table('user_project')->where('user_id', '=', $user->id)->where('project_id', '=', $project->id)) {
                         DB::table('user_project')->insert([
                             'user_id' => $user->id,
                             'project_id' => $project->id
                         ]);
                     }
                 }
-            }else{
+            } else {
                 $user = User::name($request->get('new_member'));
-                if(DB::table('user_project')->where('user_id', '=', $user->id)->where('project_id', '=', $project->id)){
+                if (DB::table('user_project')->where('user_id', '=', $user->id)->where('project_id', '=', $project->id)) {
                     DB::table('user_project')->insert([
                         'user_id' => $user->id,
                         'project_id' => $project->id
@@ -137,7 +128,7 @@ class ProjectController extends Controller
                 }
             }
             return redirect()->route('projects.show', $project->id);
-        }else{
+        } else {
             abort(403);
         }
     }
@@ -151,23 +142,24 @@ class ProjectController extends Controller
      */
     public function update(Request $request, Project $project)
     {
-        if($request->get('new_name'))
+        if ($request->get('new_name'))
             $project->name = $request->get('new_name');
-        if($request->get('new_description') !== null)
+        if ($request->get('new_description') !== null)
             $project->description = $request->get('new_description');
-        if($request->get('new_user_id') !== null)
+        if ($request->get('new_user_id') !== null)
             $project->user_id = $request->get('new_user_id');
-        if($request->get('new_is_private') !== null)
+        if ($request->get('new_is_private') !== null)
             $project->is_private = $request->get('new_is_private');
-        if($request->get('new_completed') !== null)
+        if ($request->get('new_completed') !== null)
             $project->completed = $request->get('new_completed');
-        if($request->get('new_deadline') !== null)
+        if ($request->get('new_deadline') !== null)
             $project->deadline = $request->get('new_deadline');
         $project->save();
         return redirect()->route('projects.show', $project->id);
     }
 
-    public function export($project_id){
+    public function export($project_id)
+    {
         $project = Project::find($project_id);
         LINQ::from($project->tasks)->csv("{NOW}-$project->name")->download();
     }
@@ -183,5 +175,44 @@ class ProjectController extends Controller
     {
         $project->delete();
         return redirect()->route('project.index');
+    }
+
+    /**
+     * @param Request $request
+     */
+    private function manual_create(Request $request): void
+    {
+        $project = new Project([
+            'name' => $request->get('new_name'),
+            'deadline' => $request->get('new_deadline'),
+            'user_id' => Auth::id(),
+            'description' => $request->get('new_content')
+        ]);
+        $project->save();
+        $project = Project::last();
+        DB::table('user_project')->insert([
+            'user_id' => Auth::id(),
+            'project_id' => $project->id
+        ]);
+        if (Str::contains($request->get('new_responsibles'), ',')) {
+            foreach (explode(',', $request->get('new_responsibles')) as $person) {
+                DB::table('user_project')->insert([
+                    'user_id' => $user = User::name($person)->id,
+                    'project_id' => $project->id
+                ]);
+            }
+        }
+    }
+
+    private function trello_create(Request $request)
+    {
+        if($request->get('trello_code')){
+            dd('eh');
+        }else{
+            /** @var UploadedFile $file */
+            $file = $request->file('trello_json');
+            //$file->move(storage_path('upload/json/'), $file->getClientOriginalName()); //Igazából nem is kell tárolni...
+            dd($file);
+        }
     }
 }
--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -7,48 +7,86 @@
 
 @section('content')
     <div class="container-fluid">
-        <form method="post" action="{{ route('projects.store') }}">
-            @csrf
-            <div class="row mt-2">
-                <div class="col-md-6">
-                    <label for="new_name">Project Name: </label>
-                    <input id="new_name" name="new_name" type="text" class="@error('new_name') is-invalid @enderror form-control" required>
-                    @error('new_name')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="col">
-                    <label for="new_deadline">Deadline: </label>
-                    <input id="new_deadline" name="new_deadline" type="datetime-local" class="@error('new_deadline') is-invalid @enderror form-control">
-                    @error('new_deadline')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
+        <nav>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <a class="nav-item nav-link active" id="nav-manual-tab" data-toggle="tab" href="#nav-manual" role="tab"
+                   aria-controls="nav-manual" aria-selected="true">Manual</a>
+                <a class="nav-item nav-link" id="nav-trello-tab" data-toggle="tab" href="#nav-trello" role="tab"
+                   aria-controls="nav-trello" aria-selected="false">Trello</a>
             </div>
-            <div class="row mt-2">
-                <div class="col">
-                    <label for="new_responsibles">Members: </label>
-                    <input id="new_responsibles" name="new_responsibles" type="text" class="@error('new_responsibles') is-invalid @enderror form-control">
-                    @error('new_responsibles')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-manual" role="tabpanel" aria-labelledby="nav-manual-tab">
+                <form method="post" action="{{ route('projects.store') }}">
+                    @csrf
+                    <input type="hidden" id="create_method" name="create_method" value="manual">
+                    <div class="row mt-2">
+                        <div class="col-md-6">
+                            <label for="new_name">Project Name: </label>
+                            <input id="new_name" name="new_name" type="text"
+                                   class="@error('new_name') is-invalid @enderror form-control" required>
+                            @error('new_name')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="col">
+                            <label for="new_deadline">Deadline: </label>
+                            <input id="new_deadline" name="new_deadline" type="datetime-local"
+                                   class="@error('new_deadline') is-invalid @enderror form-control">
+                            @error('new_deadline')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col">
+                            <label for="new_responsibles">Members: </label>
+                            <input id="new_responsibles" name="new_responsibles" type="text"
+                                   class="@error('new_responsibles') is-invalid @enderror form-control">
+                            @error('new_responsibles')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col">
+                            <label for="new_content">Description: </label>
+                            <textarea id="new_content" name="new_content" class="form-control"></textarea>
+                            @error('new_content')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-md-6">
+                            <button type="submit" class="btn btn-success">Create</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div class="row mt-2">
-                <div class="col">
-                    <label for="new_content">Description: </label>
-                    <textarea id="new_content" name="new_content" class="form-control"></textarea>
-                    @error('new_content')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
+            <div class="tab-pane fade" id="nav-trello" role="tabpanel" aria-labelledby="nav-trello-tab">
+                <form method="post" enctype="multipart/form-data" action="{{ route('projects.store') }}">
+                    @csrf
+                    <input type="hidden" id="create_method" name="create_method" value="trello">
+                    <div class="row mt-2">
+                        <div class="col-md-5">
+                            <input type="text" id="trello_code" name="trello_code" class="form-control" placeholder="`https://trello.com/b/vXsYLRAJ` or just `vXsYLRAJ`">
+                        </div>
+                        <div class="col-md-2 text-center align-content-center align-items-center">
+                            <h3>OR</h3>
+                        </div>
+                        <div class="col-md-5">
+                            <input type="file" id="trello_json" name="trello_json" class="form-control file" placeholder="`https://trello.com/b/vXsYLRAJ` or just `vXsYLRAJ`">
+                        </div>
+                    </div>
+                    <div class="row mt2">
+                        <div class="col text-center">
+                            <button type="submit" class="btn btn-primary">Create Project</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div class="row mt-2">
-                <div class="col-md-6">
-                    <button type="submit" class="btn btn-success">Create</button>
-                </div>
-            </div>
-        </form>
+        </div>
     </div>
 @endsection
 
